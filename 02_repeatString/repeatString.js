1a3199882710478a18d37e7458438fc6fd7898d3
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-17 09:22:50 -0700

    Set up testing environment passed all tests in exercises 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringArray = [];
+    if (num < 0) {
+        return "ERROR";
+        brake;
+    }
+    
+    for (let index = 0; index < num; index++) {
+        stringArray.push(string);
+    }
+    return stringArray.join('')
+
 
 };
 
