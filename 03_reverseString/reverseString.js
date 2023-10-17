b845d49988106a6b9cf573fa604fa9688c532808
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-17 15:03:52 -0700

    Coded exercise 03 and passed all test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+let string = input.split('');
+let reversedString = "";
+for (let index = 1; index <= string.length; index++ ){
+    reversedString += string.at(-(index))
+}
+return reversedString
 };
 
 // Do not edit below this line
