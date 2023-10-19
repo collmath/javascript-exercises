98e06d6276df18be693a0a1659dacdfc8644224d
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-19 12:54:01 -0700

    Completed exercises 04 and passed all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, args) {
+    let argCopy = Array.from(arguments);
+    let resultArray = argCopy[0]
+    argCopy.slice(1).forEach(element => {
+        resultArray = resultArray.filter((item) => item !== element);
+    });
+    return resultArray
 };
 
 // Do not edit below this line
