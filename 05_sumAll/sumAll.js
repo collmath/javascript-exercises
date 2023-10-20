13e270d9141f4572f704907eac700ba03270809f
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-20 10:13:15 -0700

    Completed exercise 05 and passed all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof(start) !== 'number' || start < 0 || 
+        end < 0 || typeof(end) !== 'number'){
+        return 'ERROR';
+    };
+    let argCopy = Array.from(arguments).sort()
+    let result = 0;
+    for (let index = argCopy[0]; index <= argCopy[1]; index++){
+    result += index};
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
