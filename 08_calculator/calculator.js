b6c54f200b9fd02b00119f8f7ae6223b8cf966c9
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-22 14:12:32 -0700

    Completed exercise 08 and passed all test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(one, two) {
+	return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+	return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(argArray) {
+  let results = 0;
+  argArray.forEach(element => {
+    results += element;
+  });
+  return Number(results);
 };
 
-const multiply = function() {
-
+const multiply = function(argArray) {
+  let results = 1;
+  argArray.forEach(element => {
+    results *= element;
+  });
+  return Number(results);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base**power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    return undefined;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    let results = 1
+    for (let index = 1; index <= num; index++) {
+      results *= index;
+    };
+    return results;
+  };
 };
 
 // Do not edit below this line
