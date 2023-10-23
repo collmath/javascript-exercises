ffb1a2cc66b6369d56db06bcd2e66bf7689799f6
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-23 11:05:30 -0700

    Completed exercise 09 and passed all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    return text.split(/[\s\W]*/).join("").toLowerCase()
+     === text.split(/[\s\W]*/).reverse().join("").toLowerCase()
 };
 
 // Do not edit below this line
