f3d6682e58a94816abf616e6958a546e1372f098
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-23 17:38:01 -0700

    Completed exercies 10 and passed all test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0) {
+        return "OOPS";
+    }else if (num === 1) {
+        return 1;
+    }else{
+        let results = [1,1]
+        for (let index = 2; index < num; index++) {
+            results.push((results[0] + results[1]));
+            results.shift();
+        };
+        return results[1];
+    }
 };
 
 // Do not edit below this line
