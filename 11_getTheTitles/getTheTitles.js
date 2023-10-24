8084b54e500f0301d89ccd2b6375159e949079af
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-24 13:14:01 -0700

    Completed exercise 11 and passed all test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let results = []
+    for (let key in Object.keys(books)) {
+        results.push(books[key].title);
+    };
+    return results;
 };
 
 // Do not edit below this line
