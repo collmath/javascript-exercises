38440e5e5dadfca6966fda5fea400fab73d2bcd0
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-23 17:49:38 -0700

    modified code to return 0 if passed 0

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,11 @@
 const fibonacci = function(num) {
-    if (num <= 0) {
+    if (num < 0) {
         return "OOPS";
-    }else if (num === 1) {
-        return 1;
+    }else if (num === 0) {
+        return 0;
     }else{
-        let results = [1,1]
-        for (let index = 2; index < num; index++) {
+        let results = [0,1]
+        for (let index = 1; index < num; index++) {
             results.push((results[0] + results[1]));
             results.shift();
         };
