bc522fece8a94eaf8bf077bd2bd7832eac845227
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-25 16:28:02 -0700

    Completed exercise 12 and passed all test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((first, next) => {
+        if((next.yearOfDeath || new Date().getFullYear()) - next.yearOfBirth 
+        > (first.yearOfDeath || new Date().getFullYear()) - first.yearOfBirth) {
+            return next;
+        };
+        return first
+    });
 };
 
 // Do not edit below this line
